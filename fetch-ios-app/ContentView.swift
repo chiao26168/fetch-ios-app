65adfc0e396d4baf8da41d9f80dca7ecb4144aca
clairelee <yuchiao0411@gmail.com>
2023-11-06 18:14:11 -0800

    Initial Commit

---
+++ fetch-ios-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fetch-ios-app
+//
+//  Created by Claire Lee on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fetch-ios-app/fetch_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_ios_appApp.swift
+//  fetch-ios-app
+//
+//  Created by Claire Lee on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct fetch_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
